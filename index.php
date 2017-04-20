bdd2515ed8a6759ee7c69712e04457efad9263d6
Wojciech <wojciech.pastuszka@gazeta.pl>
2017-04-20 17:31:26 +0200

    add showDetails and JS refactor

--- index.php
+++ index.php
@@ -13,27 +13,69 @@
             
             div {
                 background-color: slategray;
+                padding: 5px;
             }
+            
+            .hidden {
+                display: none;                
+            }
+            
         </style>
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
         <script>
-            axios.get('api/books.php').then(function (response) {
-            for (var i = 0; i < response.data.length; i++) {     
-                var section = document.createElement("section");
-                document.body.appendChild(section);
-                
+            function showTitle(parent, book) {
                 var titleEl = document.createElement("h3");
-                var title = document.createTextNode(response.data[i].title);
-                titleEl.appendChild(title);                
-                section.appendChild(titleEl);
+                var title = document.createTextNode(book.title);
+                titleEl.appendChild(title);
+                titleEl.dataset.id = book.id;
+                parent.appendChild(titleEl);
                 
                 var div = document.createElement("div");
-                section.appendChild(div);
-              }
-            })
-            .catch(function (error) {
-              console.log(error);
-            });         
+                div.classList.add("hidden");
+                parent.appendChild(div);
+            }
+            
+            function showDetails() {               
+                var url = "api/books.php?id=" + this.dataset.id;
+                var divEl = this.nextElementSibling;
+                                
+                if(divEl.className.indexOf("hidden") !== -1) {
+                    axios.get(url).then(function (response) {                        
+                        var author = document.createTextNode(response.data.author);
+                        divEl.appendChild(author);
+                        divEl.classList.remove("hidden");
+                    })
+                    .catch(function (error) {
+                         console.log(error);
+                    });
+                } else {
+                    divEl.innerText = "";
+                    divEl.classList.add("hidden");                    
+                }                
+            }
+            
+            
+            function fetchAndPrintData() {
+                axios.get('api/books.php').then(function (response) {
+                for (var i = 0; i < response.data.length; i++) {     
+                    var section = document.createElement("section");
+                    document.body.appendChild(section);
+
+                    showTitle(section, response.data[i]);               
+                }
+                
+                var titles = document.getElementsByTagName("h3");
+                for(var i = 0; i < titles.length; i++) {
+                    titles[i].addEventListener('click', showDetails);
+                }
+
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });         
+            }  
+            
+            fetchAndPrintData();
         </script>    
     </head>
     <body>
